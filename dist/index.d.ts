6e8326c16a72d32d7b74348494b23018872e4ad3
Carlos Vega <clmvega@gmail.com>
2020-05-23 17:35:56 -0600

    chore: updates type definition

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,13 +2,13 @@ export declare const encryptData: (
   data: any,
   encrypted: any,
   keys: any,
-  SEA: any
+  sea: any
 ) => Promise<any>;
 export declare const decryptData: (
   data: any,
   encrypted: any,
   keys: any,
-  SEA: any
+  sea: any
 ) => Promise<any>;
 export declare const useGun: (Gun: any, peerList: any) => any[];
 export declare const useGunNamespace: (gun: any) => any[];
@@ -17,32 +17,32 @@ export declare const useGunKeyAuth: (
   keys: any,
   triggerAuth: any
 ) => any[];
-export declare const useGunKeys: (sea: any, retrieveFn?: () => any) => any[];
+export declare const useGunKeys: (sea: any, initialState: any) => any[];
 export declare const useGunState: (
   ref: any,
   {
     appKeys,
-    SEA,
+    sea,
     interval,
     encrypted,
   }: {
     appKeys: any;
-    SEA: any;
+    sea: any;
     interval?: number;
     encrypted?: boolean;
   }
 ) => any[];
-export declare function useGunCollectionState(
+export declare const useGunCollectionState: (
   ref: any,
   {
     appKeys,
-    SEA,
+    sea,
     interval,
     encrypted,
   }: {
     appKeys: any;
-    SEA: any;
+    sea: any;
     interval?: number;
     encrypted?: boolean;
   }
-): any[];
+) => any[];
