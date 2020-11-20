0c3506c1a0730eea1f9692aeba2db03af4e894cb
Carlos Vega <clmvega@gmail.com>
2020-11-20 09:38:54 -0600

    chore: new release to fix skypack rating

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -21,22 +21,22 @@ export declare type UpdateType = {
     id: string;
     data: any;
 };
-export declare const encryptData: (data: any, keys: string | KeyPair | undefined, sea: any) => Promise<any>;
-export declare const decryptData: (data: any, keys: string | KeyPair | undefined, sea: any) => Promise<any>;
+export declare const encryptData: (data: any, keys: undefined | string | KeyPair, sea: any) => Promise<any>;
+export declare const decryptData: (data: any, keys: undefined | string | KeyPair, sea: any) => Promise<any>;
 export declare const debouncedUpdates: (dispatcher: any, timeout?: number) => (update: UpdateType) => () => void;
 export declare const reducer: (state: {}, { data, type }: ActionType) => any;
 export declare const useIsMounted: () => any;
 export declare const useSafeReducer: <T>(reducer: any, initialState: any) => [T, Function];
-export declare const useGun: (Gun: any, peerList: string[]) => any[];
-export declare const useGunNamespace: (gun: any) => any[];
-export declare const useGunKeyAuth: (gun: any, keys: KeyPair, triggerAuth?: boolean) => any[];
+export declare const useGun: (Gun: GunStatic, peerList: string[]) => any[];
+export declare const useGunNamespace: (gun: GunRef) => any[];
+export declare const useGunKeyAuth: (gun: GunRef, keys: KeyPair, triggerAuth?: boolean) => any[];
 export declare const useGunKeys: (sea: any, initialValue: any) => any[];
-export declare const useGunState: <T>(ref: any, opts?: Options) => {
+export declare const useGunState: <T>(ref: GunRef, opts?: Options) => {
     fields: T;
     put: (data: T) => Promise<void>;
     remove: (field: string) => Promise<void>;
 };
-export declare const useGunCollectionState: <T>(ref: any, opts?: Options) => {
+export declare const useGunCollectionState: <T>(ref: GunRef, opts?: Options) => {
     collection: Record<string, T>;
     addToSet: (data: T, nodeID?: string | undefined) => Promise<void>;
     updateInSet: (nodeID: string, data: T) => Promise<void>;
